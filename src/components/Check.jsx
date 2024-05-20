5fc8861f9f70c6b380686a8a24344678aa69241f
Kaspi-Chan <bozhilov53@gmail.com>
2024-05-20 17:15:33 +0300

    Add function to mark/unmark tasks as completed

--- src/components/Check.jsx
+++ src/components/Check.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import checkSvgPath from "../../public/icon-check.svg";
 
-const Check = ({ isChecked }) => {
+const Check = ({ isChecked, onClick }) => {
   return (
     <button
+      onClick={onClick}
       className={`check-btn rounded-full w-5 h-5 lg:w-6 lg:h-6 grid place-content-center ${
         isChecked ? "check-btn--checked" : "bg-inherit"
       }`}
-      disabled={isChecked}
     >
       {isChecked && <img src={checkSvgPath} alt="checkmark" />}
     </button>
--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -7,6 +7,12 @@ const ToDoList = ({ tasksList, setTaskList }) => {
     setTaskList((prevTasksList) => prevTasksList.filter((task) => task.id !== id));
   };
 
+  const markAsCompleted = (markedTask) => {
+    setTaskList((prevTasksList) =>
+      prevTasksList.map((task) => (task.id === markedTask.id ? { ...task, completed: !markedTask.completed } : task))
+    );
+  };
+
   return (
     <ul className="bg-light-very-light-gray dark:bg-dark-very-dark-desaturated-blue rounded-md shadow-md">
       {tasksList.map((task) => (
@@ -14,7 +20,7 @@ const ToDoList = ({ tasksList, setTaskList }) => {
           key={task.id}
           className="px-6 w-full flex items-center gap-3 lg:gap-6 hover-reveal bg-inherit first:rounded-tl-md first:rounded-tr-md last:rounded-bl-md last:rounded-br-md border-b border-light-very-light-grayish-blue dark:border-dark-extra-dark-grayish-blue last:border-none cursor-pointer"
         >
-          <Check isChecked={task.completed} />
+          <Check isChecked={task.completed} onClick={() => markAsCompleted(task)} />
           <p
             className={`flex-1 bg-inherit py-3.5 lg:py-5 text-sm 
             ${
